851b16ea95af25bf5c78ba6a75deb250e6f31b8e
Sergio Carvalho <sergio.carvalho@portugalmail.net>
2014-06-16 19:07:32 +0100

    Empty XDR Serializers

---
+++ syncthing/xdr/XDRLongIntegerSerializer.py
@@ -0,0 +1,10 @@
+class XDRLongIntegerSerializer(object):
+    _instance = None
+
+    def __new__(cls, *args, **kwargs): # This may be a singleton
+        if not cls._instance:
+            cls._instance = super(XDRLongIntegerSerializer, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
+    def serialize(self, value, destination):
+        pass
