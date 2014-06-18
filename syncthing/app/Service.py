db2e29287f7573b640dd524b183b7c02c42a45ce
Sergio Carvalho <sergio.carvalho@portugalmail.net>
2014-06-18 15:08:38 +0100

    Try sending a ping on connection

--- syncthing/app/Service.py
+++ syncthing/app/Service.py
@@ -1,6 +1,13 @@
 from syncthing.app.Config import Config
 from syncthing.bep.client import BEPClient
 from asyncio import Task
+from syncthing.bep.clusterconfigmessage import BEPClusterConfigMessage
+from syncthing.bep.indexmessage import BEPIndexMessage
+from syncthing.bep.requestmessage import BEPRequestMessage
+from syncthing.bep.responsemessage import BEPResponseMessage
+from syncthing.bep.pingmessage import BEPPingMessage
+from syncthing.bep.pongmessage import BEPPongMessage
+from syncthing.bep.indexupdatemessage import BEPIndexUpdateMessage
 class Service(object):
     """A syncthing.app.service is the background service that actually offers 
     the Block Exchange Protocol over the network. It is controllable via 
@@ -37,9 +44,14 @@ class Service(object):
         return []
 
     def handle_cluster_config_message(self, connection, msg):
+        print("Received a cluster config message")
         return []
 
     def handle_index_message(self, connection, msg):
+        print("Received an index message")
+        ping = BEPPingMessage()
+        connection.write(ping)
+        print("Sent a ping message")
         return []
 
     def handle_request_message(self, connection, msg):
@@ -52,5 +64,6 @@ class Service(object):
         return []
 
     def handle_pong_message(self, connection, msg):
+        print("Received a pong message")
         return []
 
