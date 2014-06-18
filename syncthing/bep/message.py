3eade5f54d99e1ec7b749f0022435b4f4438067a
Sergio Carvalho <sergio.carvalho@portugalmail.net>
2014-06-18 14:16:57 +0100

    Add id management abilities to BEPMessage

--- syncthing/bep/message.py
+++ syncthing/bep/message.py
@@ -2,8 +2,27 @@ from syncthing.bep.serializable import BEPSerializable
 from syncthing.xdr.XDRIntegerSerializer import XDRIntegerSerializer
 
 class BEPMessage(BEPSerializable):
-    def __init__(self, messageVersion, messageType, messageId):
+    _next_available_id = 0
+    @staticmethod
+    def next_available_id():
+        return BEPMessage._next_available_id
+
+    def _get_id(self):
+        return self._id
+    def _set_id(self, value):
+        if abs(value - BEPMessage._next_available_id < 2048): # If the message ID has rolled over, do not revert the rollover
+            BEPMessage._next_available_id = max(value+1, BEPMessage._next_available_id)
+        if BEPMessage._next_available_id > 4096:
+            BEPMessage._next_available_id = 0
+        self._id = value
+    def _del_id(self):
+        del self._id
+    id = property(_get_id, _set_id, _del_id, None)
+
+    def __init__(self, messageVersion, messageType, messageId=None):
         super(BEPMessage, self).__init__()
+        if messageId is None:
+            messageId = BEPMessage.next_available_id()
         self.version = messageVersion
         self.type = messageType
         self.id = messageId
