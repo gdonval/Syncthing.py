7e5973a0fb69e4f9fff0f78313596ca3cebda99a
Sergio Carvalho <sergio.carvalho@portugalmail.net>
2014-06-18 15:07:21 +0100

    New constructor signature for BEPMessage

--- syncthing/bep/pingmessage.py
+++ syncthing/bep/pingmessage.py
@@ -2,7 +2,7 @@ from syncthing.bep.message import BEPMessage
 
 class BEPPingMessage(BEPMessage):
     BEP_TYPE=4
-    def __init__(self, messageVersion, messageId):
+    def __init__(self, messageVersion=0, messageId=None):
         super(BEPPingMessage, self).__init__(messageVersion, self.BEP_TYPE, messageId)
    
     def nextUnserializer(self):
